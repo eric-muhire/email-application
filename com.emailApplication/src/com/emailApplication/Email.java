72286250e4f94d29987a20dd10d488914a6c09f8
eric-muhire <e.muhire24@gmail.com>
2021-10-07 09:48:43 +0200

    have created email and emailApplication class, aslo added a method for asking department and a  medthod for generating password

---
+++ com.emailApplication/src/com/emailApplication/Email.java
@@ -0,0 +1,17 @@
+public class Email {
+
+    String firstName;
+    String lastName;
+    String password;
+    String department;
+    int mailboxCapacity;
+    String alternateEmailAddress;
+
+
+    //Constructor to receive the first name and last name
+    //Ask for tge department
+    //Generate a random password
+    //Set the mailbox capacity
+    //AlternateEmailAddress;
+    //Change the password
+}
